df7df93c0b36a0a3aaccc8c9a3fc9dd0dc9c846e
Shai_Base <shaibase1328@gmail.com>
2024-01-28 21:18:53 +0800

    create an empty list

---
+++ Exercise-10.py
@@ -0,0 +1,12 @@
+#Create a new list from two list using the following condition
+# Given two list of numbers, write a program to create a new list
+# such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+# create lists
+first_list = [10, 20, 25, 30, 35]
+second_list = [40, 45, 60, 75, 90]
+new_list = []
+
+
+
+
